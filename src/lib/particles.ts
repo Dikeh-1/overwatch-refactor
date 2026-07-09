8b7dcec4893b67d89bfe9ddadafba31c861939b9
Dikeh-1 <ebubemichael033@gmail.com>
2026-07-09 10:57:29 +0100

    Replace particle engine with static backgrounds

--- src/lib/particles.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import type { ParticlesPluginRegistrar } from "@tsparticles/react";
-import { loadSlim } from "@tsparticles/slim";
-
-export const initParticles: ParticlesPluginRegistrar = async (engine) => {
-  await loadSlim(engine);
-};
